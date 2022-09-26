720b194992324bf2757c114352b37a5efab56825
Coin Data School <coindataschool@gmail.com>
2022-09-26 14:41:48 +0900

    implement python client for DeFiLlama's stablecoins API

--- helper/defillama.py
+++ helper/defillama.py
@@ -86,6 +86,7 @@ class DefiLlama:
         resp = self._get('TVL', f'/chains/')
         df = pd.DataFrame(resp).loc[:, ['name', 'tokenSymbol', 'tvl']]
         df = df.rename(columns={'name':'chain', 'tokenSymbol':'token'})
+        df = df.set_index('chain')
         return df
 
     def get_defi_hist_tvl(self):
@@ -193,7 +194,7 @@ class DefiLlama:
         if 'staking' in d1:
             d1.pop('staking')
         chains = list(d1.keys())
-        return {chain: self._tidy_frame(pd.DataFrame(dd['chainTvls'][chain]['tvl'])) for chain in chains}
+        return {chain: self._tidy_frame_tvl(pd.DataFrame(dd['chainTvls'][chain]['tvl'])) for chain in chains}
 
     def _tidy_frame_price(self, resp):
         # convert json response to data frame
@@ -224,8 +225,10 @@ class DefiLlama:
         ss = ','.join([v + ':' +k for k, v in token_addrs_n_chains.items()])
         resp = self._get('COINS', f'/prices/current/{ss}')
         df = self._tidy_frame_price(resp)
-        return df.loc[:, ['timestamp', 'symbol', 'price', 'confidence', 
-                          'chain', 'token_address', 'decimals']]
+        df = df.loc[:, ['timestamp', 'symbol', 'price', 'confidence', 'chain', 
+                        'token_address', 'decimals']]
+        df = df.set_index('timestamp')
+        return df
 
     def get_tokens_hist_snapshot_prices(self, token_addrs_n_chains, timestamp):
         """Get historical snapshot prices of tokens by contract address.
@@ -249,7 +252,9 @@ class DefiLlama:
         unix_ts = pd.to_datetime(timestamp).value / 1e9
         resp = self._get('COINS', f'/prices/historical/{unix_ts}/{ss}')
         df = self._tidy_frame_price(resp)
-        return df.loc[:, ['timestamp', 'symbol', 'price', 'chain', 'token_address', 'decimals']]
+        df = df.loc[:, ['timestamp', 'symbol', 'price', 'chain', 'token_address', 'decimals']]
+        df = df.set_index('timestamp')
+        return df 
 
     def get_closest_block(self, chain, timestamp):
         """Get the closest block to a timestamp.
@@ -270,4 +275,130 @@ class DefiLlama:
         df = pd.DataFrame(resp, index=range(1))
         df['timestamp'] = df.timestamp.apply(
             lambda x: dt.datetime.utcfromtimestamp(int(x)))
+        return df
+
+    def get_stablecoins_circulating(self, include_price=False):
+        """Get the circulating amounts for all stablecoins.
+
+        Parameters
+        ----------
+        include_price : logical (default=False)
+            Whether to include current stablecoin prices. Seems like it doesn't
+            do anything and the returned data doesn't return current price even 
+            set to True.
+
+        Returns 
+        -------
+        data frame
+        """
+        resp = self._get('STABLECOINS', f'/stablecoins?includePrices={include_price}')
+        lst = resp['peggedAssets']
+        
+        res = []
+        for d0 in lst:
+            _ = d0.pop('chainCirculating')
+            _ = d0.pop('chains')
+            res.append(pd.DataFrame(d0).reset_index(drop=True))
+        df = pd.concat(res)
+        df['id'] = df.id.astype(int)
+        df = df.set_index('id')
+        return df
+
+    def get_stablecoins_circulating_by_chain(self, include_price=False):
+        """Get the circulating amounts for all stablecoins, broken down by chain.
+
+        Parameters
+        ----------
+        include_price : logical (default=False)
+            Whether to include current stablecoin prices. Seems like it doesn't
+            do anything and the returned data doesn't return current price even 
+            set to True.
+
+        Returns 
+        -------
+        dictionary where the keys are stablecoin symbols and values are data frames.
+        """
+        resp = self._get('STABLECOINS', f'/stablecoins?includePrices={include_price}')
+        lst = resp['peggedAssets']
+        
+        dict_of_dfs = dict()
+        for d0 in lst:
+            d1 = d0.pop('chainCirculating')
+            _  = d0.pop('chains')
+            haha = []
+            for k, v in d1.items():
+                da = pd.DataFrame(v)
+                da['chain'] = k
+                da = da.reset_index().rename(columns={'index':'type'}).set_index('chain')
+                haha.append(da)
+            dict_of_dfs[d0['symbol']] = pd.concat(haha)
+        return dict_of_dfs
+
+    def get_stablecoin_hist_mcap(self, id):
+        """Get all available historical mcap values for a stablecoin.
+
+        Parameters
+        ----------
+        id : int 
+            Stablecoin ID, you can get these from get_stablecoins_circulating().
+            For example, USDT has id 1, USDC 2, Frax 6.
+
+        Returns 
+        -------
+        data frame
+        """
+        resp = self._get('STABLECOINS', f'/stablecoincharts/all?stablecoin={id}')
+        df = pd.concat([pd.DataFrame(d) for d in resp])        
+        df['date'] = df.date.apply(lambda x: dt.datetime.utcfromtimestamp(int(x)))
+        df = df.set_index('date')
+        return df
+
+    def get_stablecoin_hist_mcap_on_a_chain(self, id, chain):
+        """Get all available historical mcap values for a stablecoin on a 
+        particular chain.
+
+        Parameters
+        ----------
+        id : int 
+            Stablecoin ID, you can get these from get_stablecoins_circulating().
+            For example, USDT has id 1, USDC 2, Frax 6.
+        chain : string
+            Name of the chain where the stablecoin resides.
+
+        Returns 
+        -------
+        data frame
+        """
+        resp = self._get('STABLECOINS', f'/stablecoincharts/{chain}?stablecoin={id}')
+        df = pd.concat([pd.DataFrame(d) for d in resp])        
+        df['date'] = df.date.apply(lambda x: dt.datetime.utcfromtimestamp(int(x)))
+        df = df.set_index('date')
+        return df
+
+    def get_stablecoins_curr_mcap_by_chain(self):
+        """Get current mcap sum of all stablecoins on each chain.
+
+        Returns 
+        -------
+        data frame
+        """
+        resp = self._get('STABLECOINS', f'/stablecoinchains')
+        df = pd.concat([pd.DataFrame(d) for d in resp])
+        df = df.reset_index().rename(columns={'index':'type'})
+        df = df.set_index('name').drop(['gecko_id', 'tokenSymbol'], axis=1)
+        df.index.name = 'chain'
+        return df
+
+    def get_stablecoins_prices(self):
+        """Get historical prices of all stablecoins.
+
+        Returns 
+        -------
+        data frame
+        """
+        resp = self._get('STABLECOINS', f'/stablecoinprices')
+        df = pd.concat([pd.DataFrame(d) for d in resp])
+        df = df.reset_index().rename(columns={'index':'stablecoin'})
+        df['date'] = df.date.apply(lambda x: dt.datetime.utcfromtimestamp(int(x)))
+        df = df.set_index('date')
         return df
